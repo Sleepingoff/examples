c44ab004691693f7513e898b7df2f3710252c293
Sleepingoff <justjh30@gmail.com>
2023-08-21 16:25:59 +0900

    [ADD] 헤더 추가

--- starbugs/index.js
+++ starbugs/index.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", () => {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", () => {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+
+searchInputEl.addEventListener("blur", () => {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+  searchInputEl.value = "";
+});
+
 // 범위 랜덤 함수(소수점 2자리까지)
 function random(min, max) {
   // `.toFixed()`를 통해 반환된 문자 데이터를,
